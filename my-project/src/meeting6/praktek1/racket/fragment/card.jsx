30b2d6aabd86492ed2bd79dca5c6b6f65e1a9e0c
Jusuf Fathan Nuradly <jusuf1909@student.abudzar.sch.id>
2025-01-06 10:13:46 +0700

    j

--- my-project/src/meeting6/praktek1/racket/fragment/card.jsx
+++ my-project/src/meeting6/praktek1/racket/fragment/card.jsx
@@ -1,34 +1,34 @@
-import { rackets } from "../../data/data";
-import Detail from "../element/detail";
-import Images from "../element/image";
-import Button from "../element/button";
+    import { rackets } from "../../data/data";
+    import Detail from "../element/detail";
+    import Images from "../element/image";
+    import Button from "../element/button";
 
-export function Card({}) {
+    export function Card({}) {
 
-    return (
-        <div className="grid  grid-cols-4 ml-20 mb-20 ">
-            {rackets.map((racket) => (
-                <div key={racket.id} className="bg-slate-200 mt-20 w-[300px] rounded-lg shadow-2xl    " >
-                    <div className="  mx-auto">
-                        <Images className="rounded-lg"  src={racket.image} alt={racket.title}   />
+        return (
+            <div className="grid  grid-cols-4 ml-20 mb-20 ">
+                {rackets.map((racket) => (
+                    <div key={racket.id} className="bg-slate-200 mt-20 w-[300px] rounded-lg shadow-2xl    " >
+                        <div className="  mx-auto">
+                            <Images className="rounded-lg"  src={racket.image} alt={racket.title}   />
+                        </div>
+                        <div className="p-5 ">
+                            <p className="font-bold text-[16px] bg-slate-300 py-1 px-2  rounded-lg">
+                            <Detail detail={racket.title} />
+                            </p>
+                            <p className=" text-[14px] mt-3">
+                            <Detail detail={racket.price}  />
+                            </p>
+                            <p className=" text-[14px] mt-3">
+                            <Detail detail={racket.desc} />
+                            </p>
+                            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-5">
+                            <Button/>
+                            </button>
+                        </div>
                     </div>
-                    <div className="p-5 ">
-                        <p className="font-bold text-[16px] bg-slate-300 py-1 px-2  rounded-lg">
-                        <Detail detail={racket.title} />
-                        </p>
-                        <p className=" text-[14px] mt-3">
-                        <Detail detail={racket.price}  />
-                        </p>
-                        <p className=" text-[14px] mt-3">
-                        <Detail detail={racket.desc} />
-                        </p>
-                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-5">
-                        <Button/>
-                        </button>
-                    </div>
-                </div>
-            ))}
-        </div>
-       
-    );
-}
+                ))}
+            </div>
+        
+        );
+    }
